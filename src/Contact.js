1062d7cca417be84f30ef2b91c5966cdc1356b3e
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-12 00:41:52 +0800

    Adding .js Files

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const Contact = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Contact
--- src/Skills.js
+++ src/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const Skills = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Skills
