8db99601694a13bdfbe8159887327ff70a7303fc
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-12 02:17:26 +0800

    Add heading and paragraph to Home component

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <h1>Welcome to My Profile!</h1>
+      <p>Only the thing I learned here is how to convert small problem into very Big Problem.</p>
     </div>
   )
 }
