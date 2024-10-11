4d3ddc6b9091b4dc5e71a0d2dd2f30e379a71551
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-12 02:21:27 +0800

    Add heading and bulleted list of skills

--- src/Skills.js
+++ src/Skills.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Skills = () => {
   return (
     <div>
-      
+      <h2>My Skills</h2>
+      <div>
+        <ul>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>Java</li>
+        </ul>
+      </div>
     </div>
   )
 }
