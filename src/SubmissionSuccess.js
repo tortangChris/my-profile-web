e2161bccd332e0fc1a00aedc43bf3b68b8e295ce
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-13 00:47:50 +0800

    Add functionality for displaying submitted data on success page

--- src/SubmissionSuccess.js
+++ src/SubmissionSuccess.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const SubmissionSuccess = () => {
+  const location = useLocation()
+  const { name, email, message } = location.state || {};
+
   return (
     <div>
-      
+      <h1>Thank You,{name}</h1>
+      <p>Your message has been sent successfully!</p>
+
+      <h5>Submitted Information</h5>
+      <p><strong>Name:</strong> {name} </p>
+      <p><strong>Email:</strong> {email} </p>
+      <p><strong>Message:</strong> {message} </p>
     </div>
   )
 }
