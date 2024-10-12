98c71a692f5f4f27e23bdb100ea538e3ae0b4901
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-12 22:34:30 +0800

    Add heading and paragraph

--- src/About.js
+++ src/About.js
@@ -3,7 +3,14 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      
+      <h2>About Me</h2>
+      <div>
+        <p>
+            Adrenaline Rush is not a Joke. Nung naaksidente yung tricycle na sinasakyan namin,
+            madami akong natamong sugat. Pero gumapang talaga ako papunta sa Driver para kunin
+            sukli ko.
+        </p>
+      </div>
     </div>
   )
 }
