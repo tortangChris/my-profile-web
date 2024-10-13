144116a9ca2d5c04340a964aef703324b436f32b
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-13 18:18:37 +0800

    Implement card design and center align text for Skills

--- src/Skills.js
+++ src/Skills.js
@@ -2,16 +2,22 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>
-      <h2>My Skills</h2>
-      <div>
-        <ul>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>CSS</li>
-            <li>HTML</li>
-            <li>Java</li>
-        </ul>
+    <div className='container mt-5'>
+      <div className='card'>
+        <div className='card-body'>
+          <div className='text-center'>
+          <h2>My Skills</h2>
+            <div>
+              <ul className='list-unstyled'>
+                  <li className='py-2'>JavaScript</li>
+                  <li className='py-2'>React</li>
+                  <li className='py-2'>CSS</li>
+                  <li className='py-2'>HTML</li>
+                  <li className='py-2'>Java</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
