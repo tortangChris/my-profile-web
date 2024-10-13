95541c480468f55b88be09913f9df9424c81d5d1
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-13 16:37:09 +0800

    Add Card Header and Body

--- src/Contact.js
+++ src/Contact.js
@@ -15,26 +15,33 @@ const Contact = () => {
     }
 
   return (
-    <div>
-      <h2>Contact Me</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-            <label>Name</label>
-            <input type="text" id="name" value={name} onChange={(e) => inputName(e.target.value)} required />
+    <div className='container mt-5'>
+      <div className='card'>
+        <div className='card-header'>
+        <h2>Contact Me</h2>
         </div>
-        <div>
-            <label>Email</label>
-            <input type="email" id="email" value={email} onChange={(e) => inputEmail(e.target.value)} required />
+        <div className='card-body'> 
+          <form onSubmit={handleSubmit}>
+            <div className='mb-3'>
+                <label>Name</label>
+                <input type="text" id="name" value={name} onChange={(e) => inputName(e.target.value)} required />
+            </div>
+            <div className='mb-3'>
+                <label>Email</label>
+                <input type="email" id="email" value={email} onChange={(e) => inputEmail(e.target.value)} required />
+            </div>
+            <div className='mb-3'>
+                <label>Message</label>
+                <textarea id="massage" rows="5" value={message} onChange={(e) => inputMessage(e.target.value)} required />
+            </div>
+            <div>
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div>
-            <label>Message</label>
-            <textarea id="massage" rows="5" value={message} onChange={(e) => inputMessage(e.target.value)} required />
-        </div>
-        <div>
-            <button type="submit">Submit</button>
-        </div>
-      </form>
     </div>
+    
   )
 }
 
