a37ad21b91c741e82ee19920776b1cdb8272e5d2
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-13 18:14:47 +0800

    Implement card design and center align text for Home

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to My Profile!</h1>
-      <p>Only the thing I learned here is how to convert small problem into very Big Problem.</p>
+    <div className='container mt-5'>
+      <div className='card'>
+        <div className='card-body'>
+          <div className='text-center'>
+            <h1>Welcome to My Profile!</h1>
+            <p>Only the thing I learned here is how to convert small problem into very Big Problem.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
