f8b89f52fe67f1bd7f220fbb2ee8692a1c395baf
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-13 18:17:04 +0800

    Implement card design and center align text for About component

--- src/About.js
+++ src/About.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>About Me</h2>
-      <div>
-        <p>
-            Adrenaline Rush is not a Joke. Nung naaksidente yung tricycle na sinasakyan namin,
-            madami akong natamong sugat. Pero gumapang talaga ako papunta sa Driver para kunin
-            sukli ko.
-        </p>
+    <div className='container mt-5'>
+      <div className='card'>
+        <div className='card-body'>
+          <div className='text-center'>
+            <h2>About Me</h2>
+            <div>
+              <p>
+                  Adrenaline Rush is not a Joke. Nung naaksidente yung tricycle na sinasakyan namin,
+                  madami akong natamong sugat. Pero gumapang talaga ako papunta sa Driver para kunin
+                  sukli ko.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
