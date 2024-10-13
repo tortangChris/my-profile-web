5e03fa70ae33999fae57ebe33c3d09fdb385feea
tortangChris <eliseriochrisashley12@gmail.com>
2024-10-13 14:20:08 +0800

    fixing bug

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
       </nav>
       
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/home" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/skills" element={<Skills />}/>
         <Route path="/contact" element={<Contact />}/>
