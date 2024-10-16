b8a1e98f0fffac18b9534592cae8be71cbf5cd24
tortangChris <eliseriochrisashley@gmail.com>
2024-10-17 03:11:56 +0800

    Justify Bullets for Skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const Skills = () => {
   return (
     <div className='container mt-5'>
-      <div className='card bg-dark text-warning'  style={{ maxWidth: '800px', margin: 'auto' }}>
+      <div className='card bg-dark text-warning' style={{ maxWidth: '800px', margin: 'auto' }}>
         <div className='card-body'>
           <div className='text-center'>
-          <h2>My Skills</h2>
-          <hr />
-            <div>
-              <ul className='list-unstyled'>
-                  <li className='py-2'>JavaScript</li>
-                  <li className='py-2'>React</li>
-                  <li className='py-2'>CSS</li>
-                  <li className='py-2'>HTML</li>
-                  <li className='py-2'>Java</li>
-              </ul>
-            </div>
+            <h2>My Skills</h2>
+            <hr />
+            <ul className='list-unstyled'>
+              <li className='py-2 d-flex justify-content-center align-items-center'>
+                <span className='me-2'>•</span> JavaScript
+              </li>
+              <li className='py-2 d-flex justify-content-center align-items-center'>
+                <span className='me-2'>•</span> React
+              </li>
+              <li className='py-2 d-flex justify-content-center align-items-center'>
+                <span className='me-2'>•</span> CSS
+              </li>
+              <li className='py-2 d-flex justify-content-center align-items-center'>
+                <span className='me-2'>•</span> HTML
+              </li>
+              <li className='py-2 d-flex justify-content-center align-items-center'>
+                <span className='me-2'>•</span> Java
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
