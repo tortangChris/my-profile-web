0da96fab0ce637043e90e9c4cda7614b878c9876
tortangChris <eliseriochrisashley@gmail.com>
2024-10-17 02:12:59 +0800

    Add Background About Me and Add Margin

--- src/About.js
+++ src/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
@@ -7,18 +7,31 @@ const About = () => {
         <div className='card-body'>
           <div className='text-center'>
             <h2>About Me</h2>
+            <hr />
             <div>
-              <p>
-                  Adrenaline Rush is not a Joke. Nung naaksidente yung tricycle na sinasakyan namin,
-                  madami akong natamong sugat. Pero gumapang talaga ako papunta sa Driver para kunin
-                  sukli ko.
+              <p style={{ margin: '0 15%' }}>
+                I like learning to code because it’s a fun hobby for me.
+              </p>
+              <p style={{ margin: '0 15%' }}>
+                I also enjoy biking and going out to explore new places.
+              </p>
+              <p style={{ margin: '0 15%' }}>
+                I play mobile games, which help me relax and have fun.
+              </p>
+              <hr />
+              <h2 className='mt-3'>Background</h2>
+              <p style={{ margin: '0 15%' }}>
+                I am currently studying at Pamantasan ng Cabuyao for my college degree. Before this, I completed high school at AMA Computer College Calamba, having started my journey at Pulo National High School. I began my education at San Isidro Elementary School.
+              </p>
+              <p style={{ margin: '0 15%' }}>
+                Alongside my studies, I gained work experience as a Game Attendant at Enchanted Kingdom, where I interacted with guests to ensure they had fun playing the games. I also worked as a Production Worker at SPI Corporation.
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
